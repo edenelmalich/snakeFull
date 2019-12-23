fce2976331f6330adb76775ea3673b7e902e3416
eden <eelmalich2@gmail.com>
2019-12-23 15:15:29 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-
+const path = require('path');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
