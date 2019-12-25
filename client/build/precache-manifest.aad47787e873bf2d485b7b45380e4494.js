1dca5bacf4e35c5c30ddfc398cd15b52614680ef
eden <eelmalich2@gmail.com>
2019-12-25 20:38:46 +0200

    initial commit

---
+++ client/build/precache-manifest.aad47787e873bf2d485b7b45380e4494.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9535e45b035404ad020b3d8d73f8bac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b26f0b52ba73550a9018",
+    "url": "/static/css/main.1d8e3ad3.chunk.css"
+  },
+  {
+    "revision": "9811d8ea5cee9e63ea1d",
+    "url": "/static/js/2.eba0d57a.chunk.js"
+  },
+  {
+    "revision": "f1328b1bbce7a31b6321771da3fe2019",
+    "url": "/static/js/2.eba0d57a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b26f0b52ba73550a9018",
+    "url": "/static/js/main.14bb1414.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "f2ce4eacb0d0e649b4f02c02081d512e",
+    "url": "/static/media/SnakeLogo.f2ce4eac.png"
+  },
+  {
+    "revision": "308325376471319393d56186b42e0c42",
+    "url": "/static/media/apple.30832537.png"
+  },
+  {
+    "revision": "6d0adcb45cdeec9c652ff86dd7c89c7b",
+    "url": "/static/media/poison.6d0adcb4.png"
+  }
+]);
